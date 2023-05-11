de0c51560c6bfe97532d5dac64be620de412815d
Jaroslaw Romanczuk <szamotulok@gmail.com>
2023-05-12 01:27:54 +0200

    3 commit

--- src/main/java/pl/jarrobots/totalcommander/Functions.java
+++ src/main/java/pl/jarrobots/totalcommander/Functions.java
@@ -1,4 +1,39 @@
 package pl.jarrobots.totalcommander;
 
-public class Functions {
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.attribute.FileTime;
+
+public abstract class Functions {
+    public static void cp(String url, String destination, String name){
+        Path dest = Paths.get(destination);
+        Path _url = Paths.get(url);
+        try {
+            Files.copy(_url, dest, StandardCopyOption.REPLACE_EXISTING);
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void rm(String url){
+        try {
+            Files.delete(Paths.get(url));
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static RowItem add(String url, String name) throws IOException{
+        url = url+"/"+name;
+        new File(url).mkdirs();
+        FileTime time = Files.getLastModifiedTime(Paths.get(url));
+        return new RowItem(url, name, true,time);
+    }
 }
